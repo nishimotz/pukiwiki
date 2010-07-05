d27e5ba7e70fe49697f8ce33b13ab38fcf0686b2
Takuya Nishimoto <nishimotz@gmail.com>
2010-07-06 07:41:20 +0900

    pre.inc.php hyperlink support

--- plugin/pre.inc.php
+++ plugin/pre.inc.php
@@ -1,5 +1,8 @@
 <?php
-/* http://home.arino.jp/?pre.inc.php */
+/* pre.inc.php */
+/* modified by Takuya Nishimoto */
+/* original:  http://home.arino.jp/?pre.inc.php */
+/* reference: http://www.phppro.jp/qa/238 */
 function plugin_pre_convert() {
 	$args = func_get_args();
 	if (count($args) == 0) {
@@ -12,6 +15,14 @@ function plugin_pre_convert() {
 	$body = $soft
 		? make_link($body)
 		: htmlspecialchars($body);
+		$body = preg_replace_callback("/(https?:\/\/[a-zA-Z0-9_\.\/\~\%\:\#\?=&\;\-]+)/i", "urllink", $body);
 	return '<pre>' . $body . '</pre>';
 }
+
+function urllink($match) {
+  if ($match[1]) {
+    return sprintf('<a href="%1$s" target="_blank" title="%1$s">%1$s</a>', htmlspecialchars($match[1]));
+  }
+}
+
 ?>
